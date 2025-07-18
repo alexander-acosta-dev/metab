86e6dd49fb6d73afc0a2e8549036eef5df76b98c
alexander-acosta-dev <alexander.acosta@sellside.cl>
2025-07-18 15:52:51 -0400

    Update import_leads_shortcut.js

--- crm_meta_leads2/static/src/js/import_leads_shortcut.js
+++ crm_meta_leads2/static/src/js/import_leads_shortcut.js
@@ -1,27 +1,47 @@
-odoo.define('crm_meta_leads.import_shortcut', function (require) {
-    "use strict";
+/** @odoo-module **/
 
-    const publicWidget = require('web.public.widget');
-    const rpc = require('web.rpc');
+import { registry } from "@web/core/registry";
+import { useService } from "@web/core/utils/hooks";
+import { onMounted } from "@odoo/owl";
 
-    publicWidget.Widget.extend({
-        selector: 'body',
-        start: function () {
-            this._super.apply(this, arguments);
-            document.addEventListener('keydown', function (e) {
-                if (e.ctrlKey && !e.altKey && !e.metaKey) {
-                    rpc.query({
-                        model: 'crm.lead',
-                        method: 'import_meta_leads',
-                        args: [],
-                    }).then(function (result) {
-                        alert(result.message);
-                    }).catch(function (err) {
-                        console.error("Error al importar leads:", err);
-                        alert("Error al importar leads.");
-                    });
-                }
-            });
+const { Component } = owl;
+
+export class MetaLeadImportShortcut extends Component {
+    setup() {
+        this.rpc = useService("rpc");
+
+        onMounted(() => {
+            document.addEventListener("keydown", this.onKeyDown);
+        });
+    }
+
+    onKeyDown = (e) => {
+        if (e.ctrlKey && !e.altKey && !e.metaKey) {
+            this.rpc
+                .query({
+                    model: "crm.lead",
+                    method: "import_meta_leads",
+                    args: [],
+                })
+                .then((result) => {
+                    this.showNotification("Leads importados desde Meta");
+                })
+                .catch((err) => {
+                    console.error("Error al importar leads:", err);
+                    this.showNotification("Error al importar leads", true);
+                });
         }
-    });
+    };
+
+    showNotification(message, isWarning = false) {
+        this.env.services.notification.add(message, {
+            type: isWarning ? "danger" : "success",
+        });
+    }
+}
+
+// Registramos el componente como servicio del entorno backend
+registry.category("main_components").add("meta_lead_import_shortcut", {
+    Component: MetaLeadImportShortcut,
+    props: {},
 });
