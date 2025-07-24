9f2cc52e156bc87cf6db760872ffd654ff8b679c
marco.roman <marco.roman@sellside.cl>
2025-07-24 17:09:46 +0000

    Actualizar módulo

--- geo_checkin/__manifest__.py
+++ geo_checkin/__manifest__.py
@@ -27,6 +27,7 @@ la visita se realizó en el lugar correcto. Ideal para equipos de ventas en terr
     'assets': {
         'web.assets_backend': [
             'geo_checkin/static/src/js/geo_checkin.js',
+            'geo_checkin/static/src/map_view/map_renderer_patch.js',
             # 'geo_checkin/static/src/map_view/map_renderer.js',
             # 'geo_checkin/static/src/map_view/map_extension.xml',
         ],
