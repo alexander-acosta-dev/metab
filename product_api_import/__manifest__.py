3522eb6287b8f4333ff7e00ab691911b75a9de72
Alexander Acosta <alexander.acosta@sellside.cl>
2025-08-13 04:23:53 +0000

    import data api

--- product_api_import/__manifest__.py
+++ product_api_import/__manifest__.py
@@ -1,24 +1,19 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'Importar Productos desde API',
-    'version': '1.0.0',
-    'summary': 'Importa productos desde API externa',
-    'description': 'Módulo para consumir API FastAPI e importar productos a Odoo',
-    'author': 'Tu Nombre',
-    'website': 'https://www.tudominio.com',
-    'license': 'LGPL-3',
+    'version': '1.0.5',
     'depends': ['base', 'product', 'stock'],
+    'license': 'LGPL-3',
     'data': [
         'views/views.xml',
         'views/stock_picking_type_views.xml',
+        'views/menu_views.xml',  # Nuevo archivo para menús
     ],
     'assets': {
         'web.assets_backend': [
-            'product_api_import/static/src/js/api_import.js',
             'product_api_import/static/src/css/style.css',
         ],
     },
     'installable': True,
     'application': True,
-    'auto_install': False,
 }
--- product_api_import/models/stock_picking_type.py
+++ product_api_import/models/stock_picking_type.py
@@ -1,19 +1,13 @@
-from odoo import models, fields, api
+from odoo import models, fields, api, _
 
 class StockPickingType(models.Model):
     _inherit = 'stock.picking.type'
 
-    is_api_import = fields.Boolean(
-        string='Importado por API',
-        default=False,
-        help='Indica si este tipo de operación fue importado desde la API externa'
-    )
-
     def button_import_from_api(self):
-        """Método llamado por el botón en la vista árbol"""
+        """Acción para el botón de importación"""
         return {
             'type': 'ir.actions.act_window',
-            'name': 'Importar Productos desde API',
+            'name': 'Importar Productos',
             'res_model': 'product.api.import',
             'view_mode': 'form',
             'target': 'new',
